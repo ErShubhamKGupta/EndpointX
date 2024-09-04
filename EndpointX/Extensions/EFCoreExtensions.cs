32675ca47862effcc4cf7e7b688992d81c8e31fa
Shubham Kumar <shubham.7368@gmail.com>
2024-09-04 16:23:11 +0530

    I added 3 extention method to organize the program.cs class as it was messy.

---
+++ EndpointX/Extensions/EFCoreExtensions.cs
@@ -0,0 +1,18 @@
+﻿using EndpointX.Models.Data;
+using EndpointX.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndpointX.Extensions
+{
+    public static class EFCoreExtensions
+    {
+        public static IServiceCollection InjectDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
---
+++ EndpointX/Models/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace EndpointX.Models
+{
+    public class AppSettings
+    {
+        public string JWTSecretKey { get; set; }
+    }
+}
